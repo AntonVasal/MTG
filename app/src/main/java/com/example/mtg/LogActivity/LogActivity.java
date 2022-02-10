66e25ef791bcee427aad6c39dedd3210c028c6a4
Anton <avasalatij@gmail.com>
2022-02-10 08:59:37 +0200

    Morning

--- app/src/main/java/com/example/mtg/LogActivity/LogActivity.java
+++ app/src/main/java/com/example/mtg/LogActivity/LogActivity.java
@@ -1,6 +1,7 @@
 package com.example.mtg.LogActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -8,9 +9,13 @@ import com.example.mtg.R;
 
 public class LogActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.log_activity_container,new SignInFragment());
     }
 }
