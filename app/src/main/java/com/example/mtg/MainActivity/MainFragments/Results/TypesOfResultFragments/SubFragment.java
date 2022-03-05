02d712ea959e0749f726e4ce7438d7e32ff97bd4
Anton <avasalatij@gmail.com>
2022-03-06 00:07:23 +0200

    Spring Day 6 Night

--- app/src/main/java/com/example/mtg/MainActivity/MainFragments/Results/TypesOfResultFragments/SubFragment.java
+++ app/src/main/java/com/example/mtg/MainActivity/MainFragments/Results/TypesOfResultFragments/SubFragment.java
@@ -65,7 +65,7 @@ public class SubFragment extends Fragment {
     private void initListeners() {
         natButton.setOnClickListener(view -> {
             natButton.setEnabled(false);
-            natButton.setTextColor(getResources().getColor(R.color.orange_dark,null));
+            natButton.setTextColor(getResources().getColor(R.color.violet,null));
             intButton.setEnabled(true);
             intButton.setTextColor(getResources().getColor(R.color.black,null));
             decButton.setEnabled(true);
@@ -86,7 +86,7 @@ public class SubFragment extends Fragment {
             natButton.setEnabled(true);
             natButton.setTextColor(getResources().getColor(R.color.black,null));
             intButton.setEnabled(false);
-            intButton.setTextColor(getResources().getColor(R.color.orange_dark,null));
+            intButton.setTextColor(getResources().getColor(R.color.violet,null));
             decButton.setEnabled(true);
             decButton.setTextColor(getResources().getColor(R.color.black,null));
 
@@ -106,7 +106,7 @@ public class SubFragment extends Fragment {
             intButton.setEnabled(true);
             intButton.setTextColor(getResources().getColor(R.color.black,null));
             decButton.setEnabled(false);
-            decButton.setTextColor(getResources().getColor(R.color.orange_dark,null));
+            decButton.setTextColor(getResources().getColor(R.color.violet,null));
 
             List<UserResultsModel> itemList = new ArrayList<>();
             for (int i = 0; i < 15; i++) {
