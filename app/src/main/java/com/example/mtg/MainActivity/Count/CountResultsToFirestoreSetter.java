58e06b293f475674f674fc22163538a79787925f
Anton <avasalatij@gmail.com>
2022-03-08 13:43:59 +0200

    Spring Day 8 / 2

--- app/src/main/java/com/example/mtg/MainActivity/Count/CountResultsToFirestoreSetter.java
+++ app/src/main/java/com/example/mtg/MainActivity/Count/CountResultsToFirestoreSetter.java
@@ -3,7 +3,9 @@ package com.example.mtg.MainActivity.Count;
 import android.util.Log;
 
 import com.example.mtg.MainActivity.Count.CountModels.AddResultsModel;
+import com.example.mtg.MainActivity.Count.CountModels.DivResultsModel;
 import com.example.mtg.MainActivity.Count.CountModels.MultiResultsModel;
+import com.example.mtg.MainActivity.Count.CountModels.SubResultsModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -83,22 +85,231 @@ public class CountResultsToFirestoreSetter {
     }
 
     private void setDivDecimalResults(int score, int tasksAmount, String userID) {
-
+        new Thread(() -> mFirebaseFirestore.collection("div")
+                .document(userID)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    DivResultsModel divResultsModel = documentSnapshot
+                            .toObject(DivResultsModel.class);
+                    if (divResultsModel == null) {
+                        divResultsModel = new DivResultsModel(0, 0, 0, 0, score, tasksAmount, userID);
+                        mFirebaseFirestore.collection("div").document(userID)
+                                .set(divResultsModel).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.i(TAG, SUCCESS);
+                            } else {
+                                Log.i(TAG, FAILED);
+                            }
+                        });
+                    } else {
+                        int oldScore = divResultsModel.getDivDecimalScore();
+                        if (score > oldScore) {
+                            divResultsModel.setDivDecimalScore(score);
+                            divResultsModel.setDivDecimalTasksAmount(tasksAmount);
+                            mFirebaseFirestore.collection("div")
+                                    .document(userID)
+                                    .set(divResultsModel)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            Log.i(TAG, SUCCESS);
+                                        } else {
+                                            Log.i(TAG, FAILED);
+                                        }
+                                    });
+                        }
+                    }
+                })
+        ).start();
     }
 
     private void setDivIntegerResults(int score, int tasksAmount, String userID) {
+        new Thread(() -> mFirebaseFirestore.collection("div")
+                .document(userID)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    DivResultsModel divResultsModel = documentSnapshot
+                            .toObject(DivResultsModel.class);
+                    if (divResultsModel == null) {
+                        divResultsModel = new DivResultsModel(0, 0, score, tasksAmount, 0, 0, userID);
+                        mFirebaseFirestore.collection("div").document(userID)
+                                .set(divResultsModel).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.i(TAG, SUCCESS);
+                            } else {
+                                Log.i(TAG, FAILED);
+                            }
+                        });
+                    } else {
+                        int oldScore = divResultsModel.getDivIntegerScore();
+                        if (score > oldScore) {
+                            divResultsModel.setDivIntegerScore(score);
+                            divResultsModel.setDivIntegerTasksAmount(tasksAmount);
+                            mFirebaseFirestore.collection("div")
+                                    .document(userID)
+                                    .set(divResultsModel)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            Log.i(TAG, SUCCESS);
+                                        } else {
+                                            Log.i(TAG, FAILED);
+                                        }
+                                    });
+                        }
+                    }
+                })
+        ).start();
     }
 
     private void setDivNaturalResults(int score, int tasksAmount, String userID) {
+        new Thread(() -> mFirebaseFirestore.collection("div")
+                .document(userID)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    DivResultsModel divResultsModel = documentSnapshot
+                            .toObject(DivResultsModel.class);
+                    if (divResultsModel == null) {
+                        divResultsModel = new DivResultsModel(score, tasksAmount, 0, 0, 0, 0, userID);
+                        mFirebaseFirestore.collection("div").document(userID)
+                                .set(divResultsModel).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.i(TAG, SUCCESS);
+                            } else {
+                                Log.i(TAG, FAILED);
+                            }
+                        });
+                    } else {
+                        int oldScore = divResultsModel.getDivNaturalScore();
+                        if (score > oldScore) {
+                            divResultsModel.setDivNaturalScore(score);
+                            divResultsModel.setDivNaturalTasksAmount(tasksAmount);
+                            mFirebaseFirestore.collection("div")
+                                    .document(userID)
+                                    .set(divResultsModel)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            Log.i(TAG, SUCCESS);
+                                        } else {
+                                            Log.i(TAG, FAILED);
+                                        }
+                                    });
+                        }
+                    }
+                })
+        ).start();
     }
 
     private void setSubDecimalResults(int score, int tasksAmount, String userID) {
+        new Thread(() -> mFirebaseFirestore.collection("sub")
+                .document(userID)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    SubResultsModel subResultsModel = documentSnapshot
+                            .toObject(SubResultsModel.class);
+                    if (subResultsModel == null) {
+                        subResultsModel = new SubResultsModel(0, 0, 0, 0, score, tasksAmount, userID);
+                        mFirebaseFirestore.collection("sub").document(userID)
+                                .set(subResultsModel).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.i(TAG, SUCCESS);
+                            } else {
+                                Log.i(TAG, FAILED);
+                            }
+                        });
+                    } else {
+                        int oldScore = subResultsModel.getSubDecimalScore();
+                        if (score > oldScore) {
+                            subResultsModel.setSubDecimalScore(score);
+                            subResultsModel.setSubDecimalTasksAmount(tasksAmount);
+                            mFirebaseFirestore.collection("sub")
+                                    .document(userID)
+                                    .set(subResultsModel)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            Log.i(TAG, SUCCESS);
+                                        } else {
+                                            Log.i(TAG, FAILED);
+                                        }
+                                    });
+                        }
+                    }
+                })
+        ).start();
     }
 
     private void setSubIntegerResults(int score, int tasksAmount, String userID) {
+        new Thread(() -> mFirebaseFirestore.collection("sub")
+                .document(userID)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    SubResultsModel subResultsModel = documentSnapshot
+                            .toObject(SubResultsModel.class);
+                    if (subResultsModel == null) {
+                        subResultsModel = new SubResultsModel(0, 0, score, tasksAmount, 0, 0, userID);
+                        mFirebaseFirestore.collection("sub").document(userID)
+                                .set(subResultsModel).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.i(TAG, SUCCESS);
+                            } else {
+                                Log.i(TAG, FAILED);
+                            }
+                        });
+                    } else {
+                        int oldScore = subResultsModel.getSubIntegerScore();
+                        if (score > oldScore) {
+                            subResultsModel.setSubIntegerScore(score);
+                            subResultsModel.setSubIntegerTasksAmount(tasksAmount);
+                            mFirebaseFirestore.collection("sub")
+                                    .document(userID)
+                                    .set(subResultsModel)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            Log.i(TAG, SUCCESS);
+                                        } else {
+                                            Log.i(TAG, FAILED);
+                                        }
+                                    });
+                        }
+                    }
+                })
+        ).start();
     }
 
     private void setSubNaturalResults(int score, int tasksAmount, String userID) {
+        new Thread(() -> mFirebaseFirestore.collection("sub")
+                .document(userID)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    SubResultsModel subResultsModel = documentSnapshot
+                            .toObject(SubResultsModel.class);
+                    if (subResultsModel == null) {
+                        subResultsModel = new SubResultsModel(score, tasksAmount, 0, 0, 0, 0, userID);
+                        mFirebaseFirestore.collection("sub").document(userID)
+                                .set(subResultsModel).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.i(TAG, SUCCESS);
+                            } else {
+                                Log.i(TAG, FAILED);
+                            }
+                        });
+                    } else {
+                        int oldScore = subResultsModel.getSubNaturalScore();
+                        if (score > oldScore) {
+                            subResultsModel.setSubNaturalScore(score);
+                            subResultsModel.setSubNaturalTasksAmount(tasksAmount);
+                            mFirebaseFirestore.collection("sub")
+                                    .document(userID)
+                                    .set(subResultsModel)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            Log.i(TAG, SUCCESS);
+                                        } else {
+                                            Log.i(TAG, FAILED);
+                                        }
+                                    });
+                        }
+                    }
+                })
+        ).start();
     }
 
     private void setMultiDecimalResults(int score, int tasksAmount, String userID) {
